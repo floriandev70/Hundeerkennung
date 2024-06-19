a01e28a0d1f9b48e270c7e29dcf50fe0865f69a8
floriancarstens <florian.carstens@hey.com>
2024-06-20 00:57:34 +0200

    images.py: 1. Train or load model 2. Use extra test image folder

--- images.py
+++ images.py
@@ -10,25 +10,22 @@ from tensorflow.keras.models import Sequential
 
 print(tf.__version__)
 
-"""### Download the flowers dataset
+EPOCHS = 20
+TRAIN_MODEL = False
+LOAD_MODEL = not TRAIN_MODEL
 
-This tutorial uses a dataset of several thousand photos of flowers. The flowers dataset contains five sub-directories, one per class:
-
-```
-flowers_photos/
-  daisy/
-  dandelion/
-  roses/
-  sunflowers/
-  tulips/
-```
-
-Note: all images are licensed CC-BY, creators are listed in the LICENSE.txt file.
-"""
+batch_size = 32
+img_height = 180
+img_width = 180
 
 import pathlib
+
+
+
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 ### TODO:CHANGE THIS TO MY NEW PATH! ###
+### Reicht wenn ich eine ähnliche Verzeichnisstruktur für meine Fotos nutze
+### --> Klassen + Labels werden automatisch gebildet ;-)
 archive = '/Users/floriancarstens/.keras/datasets/flower_photos' #tf.keras.utils.get_file(origin=dataset_url, extract=True)
 data_dir = pathlib.Path(archive).with_suffix('')
 print(data_dir)
@@ -36,28 +33,7 @@ print(data_dir)
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
 
-"""Each directory contains images of that type of flower. Here are some roses:"""
-
-roses = list(data_dir.glob('roses/*'))
-PIL.Image.open(str(roses[0]))
-
-roses = list(data_dir.glob('roses/*'))
-PIL.Image.open(str(roses[1]))
-
-"""## Load data using a Keras utility
-
-Let's load these images off disk using the helpful `tf.keras.utils.image_dataset_from_directory` utility.
-
 ### Create a dataset
-
-Define some parameters for the loader:
-"""
-
-batch_size = 32
-img_height = 180
-img_width = 180
-
-"""It's good practice to use a validation split when developing your model. You will use 80% of the images for training and 20% for validation."""
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
@@ -80,168 +56,164 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
 class_names = train_ds.class_names
 print(class_names)
 
-"""### Visualize the data
+if TRAIN_MODEL:
+  """### Visualize the data
+  
+  Here are the first nine images from the training dataset.
+  """
 
-Here are the first nine images from the training dataset.
-"""
+  print('show images')
+  plt.figure(figsize=(10, 10))
+  for images, labels in train_ds.take(1):
+    for i in range(9):
+      ax = plt.subplot(3, 3, i + 1)
+      plt.imshow(images[i].numpy().astype("uint8"))
+      plt.title(class_names[labels[i]])
+      plt.axis("off")
+  plt.show()
 
-print('show images')
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
-plt.show()
+  ## augmentation
+  data_augmentation = keras.Sequential(
+    [
+      layers.RandomFlip("horizontal",
+                        input_shape=(img_height,
+                                    img_width,
+                                    3)),
+      layers.RandomRotation(0.1),
+      layers.RandomZoom(0.1),
+      layers.RandomCrop(img_height,img_width),
+    ]
+  )
 
-## augmentation
-data_augmentation = keras.Sequential(
-  [
-    layers.RandomFlip("horizontal",
-                      input_shape=(img_height,
-                                  img_width,
-                                  3)),
-    layers.RandomRotation(0.1),
-    layers.RandomZoom(0.1),
-    layers.RandomCrop(img_height,img_width),
-  ]
-)
+  #show augmented images
+  print('augmented images 1')
+  plt.figure(figsize=(10, 10))
+  for images, _ in train_ds.take(1):
+    for i in range(9):
+      augmented_images = data_augmentation(images)
+      ax = plt.subplot(3, 3, i + 1)
+      plt.imshow(augmented_images[0].numpy().astype("uint8"))
+      plt.axis("off")
+  plt.show()
 
-#show augmented images
-print('augmented images 1')
-plt.figure(figsize=(10, 10))
-for images, _ in train_ds.take(1):
-  for i in range(9):
-    augmented_images = data_augmentation(images)
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(augmented_images[0].numpy().astype("uint8"))
-    plt.axis("off")
-plt.show()
+  print('augmented images 2')
+  plt.figure(figsize=(10, 10))
+  for images, _ in train_ds.take(2):
+    for i in range(9):
+      augmented_images = data_augmentation(images)
+      ax = plt.subplot(3, 3, i + 1)
+      plt.imshow(augmented_images[0].numpy().astype("uint8"))
+      plt.axis("off")
+  plt.show()
 
-print('augmented images 2')
-plt.figure(figsize=(10, 10))
-for images, _ in train_ds.take(2):
-  for i in range(9):
-    augmented_images = data_augmentation(images)
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(augmented_images[0].numpy().astype("uint8"))
-    plt.axis("off")
-plt.show()
+  """You can train a model using these datasets by passing them to `model.fit` (shown later in this tutorial). If you like, you can also manually iterate over the dataset and retrieve batches of images:"""
 
-"""You can train a model using these datasets by passing them to `model.fit` (shown later in this tutorial). If you like, you can also manually iterate over the dataset and retrieve batches of images:"""
+  for image_batch, labels_batch in train_ds:
+    print(image_batch.shape)
+    print(labels_batch.shape)
+    break
 
-for image_batch, labels_batch in train_ds:
-  print(image_batch.shape)
-  print(labels_batch.shape)
-  break
+  """The `image_batch` is a tensor of the shape `(32, 180, 180, 3)`. This is a batch of 32 images of shape `180x180x3` (the last dimension refers to color channels RGB). The `label_batch` is a tensor of the shape `(32,)`, these are corresponding labels to the 32 images.
+  
+  You can call `.numpy()` on either of these tensors to convert them to a `numpy.ndarray`.
+  
+  ### Standardize the data
+  
+  The RGB channel values are in the `[0, 255]` range. This is not ideal for a neural network; in general you should seek to make your input values small.
+  
+  Here, you will standardize values to be in the `[0, 1]` range by using `tf.keras.layers.Rescaling`:
+  """
 
-"""The `image_batch` is a tensor of the shape `(32, 180, 180, 3)`. This is a batch of 32 images of shape `180x180x3` (the last dimension refers to color channels RGB). The `label_batch` is a tensor of the shape `(32,)`, these are corresponding labels to the 32 images.
+  normalization_layer = tf.keras.layers.Rescaling(1./255)
 
-You can call `.numpy()` on either of these tensors to convert them to a `numpy.ndarray`.
+  """There are two ways to use this layer. You can apply it to the dataset by calling `Dataset.map`:"""
 
-### Standardize the data
-
-The RGB channel values are in the `[0, 255]` range. This is not ideal for a neural network; in general you should seek to make your input values small.
-
-Here, you will standardize values to be in the `[0, 1]` range by using `tf.keras.layers.Rescaling`:
-"""
-
-normalization_layer = tf.keras.layers.Rescaling(1./255)
-
-"""There are two ways to use this layer. You can apply it to the dataset by calling `Dataset.map`:"""
-
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-# Notice the pixel values are now in `[0,1]`.
-print(np.min(first_image), np.max(first_image))
-
-"""Or, you can include the layer inside your model definition to simplify deployment. You will use the second approach here.
-
-Note: If you would like to scale pixel values to `[-1,1]` you can instead write `tf.keras.layers.Rescaling(1./127.5, offset=-1)`
-
-Note: You previously resized images using the `image_size` argument of `tf.keras.utils.image_dataset_from_directory`. If you want to include the resizing logic in your model as well, you can use the `tf.keras.layers.Resizing` layer.
-
-### Configure the dataset for performance
-
-Let's make sure to use buffered prefetching so you can yield data from disk without having I/O become blocking. These are two important methods you should use when loading data:
-
-- `Dataset.cache` keeps the images in memory after they're loaded off disk during the first epoch. This will ensure the dataset does not become a bottleneck while training your model. If your dataset is too large to fit into memory, you can also use this method to create a performant on-disk cache.
-- `Dataset.prefetch` overlaps data preprocessing and model execution while training.
-
-Interested readers can learn more about both methods, as well as how to cache data to disk in the *Prefetching* section of the [Better performance with the tf.data API](../../guide/data_performance.ipynb) guide.
-"""
-
-AUTOTUNE = tf.data.AUTOTUNE
-
-train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-"""### Train a model
-
-For completeness, you will show how to train a simple model using the datasets you have just prepared.
-
-The [Sequential](https://www.tensorflow.org/guide/keras/sequential_model) model consists of three convolution blocks (`tf.keras.layers.Conv2D`) with a max pooling layer (`tf.keras.layers.MaxPooling2D`) in each of them. There's a fully-connected layer (`tf.keras.layers.Dense`) with 128 units on top of it that is activated by a ReLU activation function (`'relu'`). This model has not been tuned in any way—the goal is to show you the mechanics using the datasets you just created. To learn more about image classification, visit the [Image classification](../images/classification.ipynb) tutorial.
-"""
-
-num_classes = 5
-
-model = tf.keras.Sequential([
-  data_augmentation,
-  tf.keras.layers.Rescaling(1./255),
-  tf.keras.layers.Conv2D(32, 3, activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Conv2D(32, 3, activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Conv2D(32, 3, activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(num_classes)
-])
-
-# compile model
-model.compile(
-  optimizer='adam',
-  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-  metrics=['accuracy'])
-
-"""Note: You will only train for a few epochs so this tutorial runs quickly."""
-# train model
-EPOCHS = 1
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=EPOCHS
-)
-
-# TODO: save model or load model
+  normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+  image_batch, labels_batch = next(iter(normalized_ds))
+  first_image = image_batch[0]
+  # Notice the pixel values are now in `[0,1]`.
+  print(np.min(first_image), np.max(first_image))
 
 
-# visualize training results
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+  AUTOTUNE = tf.data.AUTOTUNE
 
-epochs_range = range(EPOCHS)
+  train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+  val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+  """### Train a model
+  
+  For completeness, you will show how to train a simple model using the datasets you have just prepared.
+  
+  The [Sequential](https://www.tensorflow.org/guide/keras/sequential_model) model consists of three convolution blocks (`tf.keras.layers.Conv2D`) with a max pooling layer (`tf.keras.layers.MaxPooling2D`) in each of them. There's a fully-connected layer (`tf.keras.layers.Dense`) with 128 units on top of it that is activated by a ReLU activation function (`'relu'`). This model has not been tuned in any way—the goal is to show you the mechanics using the datasets you just created. To learn more about image classification, visit the [Image classification](../images/classification.ipynb) tutorial.
+  """
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
+  print('Training model...')
+
+  num_classes = 5
+
+  model = tf.keras.Sequential([
+    data_augmentation,
+    tf.keras.layers.Rescaling(1./255),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(num_classes)
+  ])
+
+  # compile model
+  model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+  """Note: You will only train for a few epochs so this tutorial runs quickly."""
+  # train model
+  history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=EPOCHS
+  )
+
+  # visualize training results
+  acc = history.history['accuracy']
+  val_acc = history.history['val_accuracy']
+
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  epochs_range = range(EPOCHS)
+
+  plt.figure(figsize=(8, 8))
+  plt.subplot(1, 2, 1)
+  plt.plot(epochs_range, acc, label='Training Accuracy')
+  plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+  plt.legend(loc='lower right')
+  plt.title('Training and Validation Accuracy')
+
+  plt.subplot(1, 2, 2)
+  plt.plot(epochs_range, loss, label='Training Loss')
+  plt.plot(epochs_range, val_loss, label='Validation Loss')
+  plt.legend(loc='upper right')
+  plt.title('Training and Validation Loss')
+  plt.show()
+
+  # save model
+  #!mkdir -p saved_model
+  model.save('saved_model/my_model')
+
+if LOAD_MODEL:
+  print("Loading model...")
+  model = tf.keras.models.load_model('saved_model/my_model')
+
+  # Check its architecture
+  #model.summary()
 
 #############################################################################
 # try network out
@@ -249,11 +221,18 @@ plt.show()
 
 # take pic
 sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
-sunflower_path = tf.keras.utils.get_file('Red_sunflower.jpg', origin=sunflower_url)
-
+sunflower_path = '/Users/floriancarstens/.keras/test_pics/Red_sunflower.jpg'#tf.keras.utils.get_file('Red_sunflower.jpg', origin=sunflower_url, cache_subdir='test_pics')
 # create image array
+# image path
+img_path = '/Users/floriancarstens/.keras/test_pics/'
+# image filename
+img_filename = 'Dandelion3.png'#'Red_sunflower.jpg'
+# image path + name
+img_path_filename = os.path.join(img_path, img_filename)
+print(img_path_filename)
+# load image
 img = tf.keras.utils.load_img(
-    sunflower_path, target_size=(img_height, img_width)
+    img_path_filename, target_size=(img_height, img_width)
 )
 img_array = tf.keras.utils.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0) # Create a batch
@@ -266,3 +245,33 @@ print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(class_names[np.argmax(score)], 100 * np.max(score))
 )
+
+#############################################################################
+# save model or load lite model
+#############################################################################
+# Convert the Keras Sequential model to a TensorFlow Lite model which can run on Jetson MC
+#converter = tf.lite.TFLiteConverter.from_keras_model(model)
+#tflite_model = converter.convert()
+
+# Save the model.
+#with open('model.tflite', 'wb') as f:
+#  f.write(tflite_model)
+
+# run the light model
+#TF_MODEL_FILE_PATH = 'model.tflite' # The default path to the saved TensorFlow Lite model
+#interpreter = tf.lite.Interpreter(model_path=TF_MODEL_FILE_PATH)
+
+#interpreter.get_signature_list()
+
+#classify_lite = interpreter.get_signature_runner('serving_default')
+#classify_lite
+
+#predictions_lite = classify_lite(sequential_1_input=img_array)['outputs']
+#score_lite = tf.nn.softmax(predictions_lite)
+# say prediction
+#print(
+#    "This image most likely belongs to {} with a {:.2f} percent confidence."
+#    .format(class_names[np.argmax(score_lite)], 100 * np.max(score_lite))
+#)
+# compare with original model
+#print(np.max(np.abs(predictions - predictions_lite)))
